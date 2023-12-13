613f73fb0ab852929b37bbc1d7b5117e5d8c3ddb
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2023-12-13 23:11:19 +0100

    docs: Add example usage (#13)
    * docs: Add minimal usage example
    
    * docs: Add class example

---
+++ example/src/main.mjs
@@ -0,0 +1,29 @@
+import {getUserData, updateRanking} from "greengame-api-client";
+
+class ClassUsageExample {
+    constructor() {
+    }
+
+    async getUserData() {
+        // API client functions can be called inside async methods
+        const userData = await getUserData();
+        console.log(userData);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const classUsageExample = new ClassUsageExample();
+
+    const getUserDataButton = document.getElementById("btn-get-user-data");
+    const updateRankingButton = document.getElementById("btn-update-ranking");
+
+    getUserDataButton.addEventListener("click", async () => {
+        await classUsageExample.getUserData();
+    });
+    updateRankingButton.addEventListener("click", async () => {
+        // Or they can be called just as-is, as long as they're within async functions
+        await updateRanking({gameID: 1, score: 100});
+    });
+
+    classUsageExample.getUserData();
+});
