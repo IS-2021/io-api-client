c0bb89cf32fc4cac0ed38595b04ffcce00506348
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2024-01-05 20:59:46 +0100

    Feat/datastorage (#14)
    * feat: Add data storage support
    
    * docs: Add example usage
    
    * fix: Fix typing
    
    * style: Reformat files
    
    * chore: Add changeset

--- example/src/main.mjs
+++ example/src/main.mjs
@@ -1,4 +1,4 @@
-import {getUserData, updateRanking} from "greengame-api-client";
+import {getUserData, updateRanking, saveGameData, loadGameData} from "greengame-api-client";
 
 class ClassUsageExample {
     constructor() {
@@ -8,6 +8,14 @@ class ClassUsageExample {
         // API client functions can be called inside async methods
         const userData = await getUserData();
         console.log(userData);
+    }
+}
+
+async function pringGameData(gameId) {
+    // const gameData = await loadGameData({ gameId });  // This is the shorter syntax
+    const gameData = await loadGameData({ gameId: gameId });
+    if (gameData != null) {
+        console.log(gameData);
     }
 }
 
@@ -26,4 +34,30 @@ document.addEventListener("DOMContentLoaded", async () => {
     });
 
     await classUsageExample.getUserData();
+
+
+    // Data storing example
+    // gameData can be anything that can be serialized with JSON.stringify()
+    // It can be a complex object
+    await saveGameData({
+        gameId: 1,
+        gameData: {
+            unlockedLevel: 1,
+            boughtUpgrades: ["freezeGun"],
+            stats: {
+                enemiesDefeated: {
+                    bugs: 42,
+                },
+            },
+        },
+    });
+    // or just a simple (serializable) value
+    const userLastLevel = 2;
+    await saveGameData({
+        gameId: 5,
+        gameData: userLastLevel,
+    });
+
+    pringGameData(1);
+    pringGameData(5);
 });
--- src/types.ts
+++ src/types.ts
@@ -11,3 +11,16 @@ export type UpdateRankingRequest = {
 	gameID: number;
 	score: number;
 };
+
+export type SaveGameDataRequest = {
+	gameId: number;
+	gameData: unknown;
+};
+
+export type LoadGameDataRequest = {
+	gameId: number;
+};
+
+export type LoadGameDataResponse = {
+	gameDataJSON: string | undefined;
+};
